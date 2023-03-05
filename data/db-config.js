3702c3f5171ddb8f2de84b949a6307e35b813575
Alita1093 <furkanozturk1635@gmail.com>
2023-03-06 00:12:48 +0300

    s14-g4

---
+++ data/db-config.js
@@ -0,0 +1,6 @@
+const knex = require("knex");
+const configFile = require("../knexfile");
+
+const environment = "development";
+
+module.exports = knex(configFile[environment]);
---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require("./api/server");
+
+const port = process.env.PORT || 9001;
+
+server.listen(port, () => {
+  console.log("listening on ", port);
+});
